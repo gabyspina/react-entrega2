83657b3f57d34003b4510994068464504ff526f7
gabyspina <gabyspina@gmail.com>
2022-06-03 23:31:02 -0300

    agrego navbar y navbar css

---
+++ src/navbar/navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Alimentos</li>
+                <li>Accesorios</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )
+}
